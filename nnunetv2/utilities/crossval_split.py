abf95ad8664ecbcb81b9a8c0a3b3abb97be8efec
lqqqq-sayhi <liako@foxmail.com>
2025-06-08 23:34:45 +0800

    update nnUNet 202506082334

--- nnunetv2/utilities/crossval_split.py
+++ nnunetv2/utilities/crossval_split.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import KFold
 
 
-def generate_crossval_split(train_identifiers: List[str], seed=12345, n_splits=5) -> List[dict[str, List[str]]]:
+def generate_crossval_split(train_identifiers: List[str], seed=301, n_splits=5) -> List[dict[str, List[str]]]:
     splits = []
     kfold = KFold(n_splits=n_splits, shuffle=True, random_state=seed)
     for i, (train_idx, test_idx) in enumerate(kfold.split(train_identifiers)):
